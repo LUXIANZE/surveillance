1266edd22d7fcfb0c9201f2ca14eacc3f0d948d8
LUXIANZE <luxianze98@gmail.com>
2023-07-14 11:32:29 +0800

    Add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
+import { Button, ConfigProvider } from 'antd';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+const App: React.FC = () => (
+  <ConfigProvider theme={{ token: { colorPrimary: '#00b96b' } }}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="ghost">Button</Button>
     </div>
-  );
-}
+  </ConfigProvider>
+);
 
 export default App;
